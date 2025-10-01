66c2747f5cefcc4a5d95bc5aa2737bca5f697743
Cyber crime <buvanaeshwaransd@gmail.com>
2025-10-01 13:51:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -345,7 +345,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server running on port ${PORT}`);
 });
